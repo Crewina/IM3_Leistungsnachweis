3a7321144c6f590d4a73d699805f94ab281c1fe2
tanjajulmy99 <150014063+tanjajulmy99@users.noreply.github.com>
2024-10-14 21:15:43 +0200

    V4

--- etl/questionnaire.php
+++ etl/questionnaire.php
@@ -1,76 +1,84 @@
 <?php
-    require_once 'config.php'; // Stellen Sie sicher, dass dies auf Ihre tatsächliche Konfigurationsdatei verweist
+require_once 'config.php'; // Stellen Sie sicher, dass dies auf Ihre tatsächliche Konfigurationsdatei verweist
 
-    header('Content-Type: application/json');
+header('Content-Type: application/json');
 
-    class Question {
-        public $question;
-        public $options;
-        public $correct;
-        public $evaluationText;
-        public $result;
+class Question {
+    public $question;
+    public $options;
+    public $correct;
+    public $evaluationText;
+    public $result;
 
-        public function __construct($question, $options, $correct, $evaluationText, $result) {
-            $this->question = $question;
-            $this->options = $options;
-            $this->correct = $correct;
-            $this->evaluationText = $evaluationText;
-            $this->result = $result;
-        }
+    public function __construct($question, $options, $correct, $evaluationText, $result) {
+        $this->question = $question;
+        $this->options = $options;
+        $this->correct = $correct;
+        $this->evaluationText = $evaluationText;
+        $this->result = $result;
+    }
+}
+
+function formatName($name) {
+    return ucwords(strtolower($name)); // Konvertiere den gesamten Text in Kleinbuchstaben und dann jeden ersten Buchstaben eines Wortes in Großbuchstaben
+}
+
+function get_question_1($pdo) {
+    $stmt = $pdo->prepare("SELECT artist as artist_name, COUNT(*) AS amount FROM songs_SRF WHERE artist <> \"\" GROUP BY artist ORDER BY amount DESC LIMIT 4;");
+    $stmt->execute();
+    $results = $stmt->fetchAll();
+
+    // Formatieren aller Künstlernamen
+    foreach ($results as &$result) {
+        $result["artist_name"] = formatName($result["artist_name"]);
     }
 
+    $mostListenedArtist = current($results);
+    $mostListenedArtistName = $mostListenedArtist["artist_name"];
+    
+    // Den Index der richtigen Antwort ermitteln
+    $correctIndex = array_search($mostListenedArtistName, array_column($results, 'artist_name'));
 
-    function get_question_1($pdo) {
-        $stmt = $pdo->prepare("SELECT artist as artist_name, COUNT(*) AS amount
-        FROM songs_SRF
-        WHERE artist <> \"\"
-        GROUP BY artist
-        ORDER BY amount DESC
-        LIMIT 4;");
-        $stmt->execute();
-        $results = $stmt->fetchAll();
+    $answerText = "$mostListenedArtistName wird seit dem 8.10.24 am meisten auf SRF 3 gespielt.";
+    
+    return new Question(
+        "Welcher Künstler wird seit dem 8.10.24 am meisten auf SRF 3 gespielt?",
+        array_map(function($a) { return $a["artist_name"]; }, $results),
+        $correctIndex, // Setze den Index der richtigen Antwort
+        $answerText,
+        array_map(function($a) { return ['amount' => $a["amount"], 'label' => $a["artist_name"]]; }, $results)
+    );
+}
 
+function get_question_2($pdo, $artist_name) {
+    $stmt = $pdo->prepare("SELECT song as song_name, COUNT(*) AS amount FROM songs_SRF WHERE artist = '$artist_name' GROUP BY song_name ORDER BY amount DESC LIMIT 4;");
+    $stmt->execute();
+    $results = $stmt->fetchAll();
 
-        $mostListenedArtist = current($results);
-        $mostListenedArtistName = $mostListenedArtist["artist_name"];
-        $answerText = "$mostListenedArtistName wird seit dem 8.10.24 am meisten auf SRF 3 gespielt.";
-        return new Question(
-            "Welcher Künstler wird seit dem 8.10.24 am meisten auf SRF 3 gespielt?",
-            array_map(function($a) { return $a["artist_name"]; }, $results),
-            $mostListenedArtistName,
-            $answerText,
-            array_map(function($a) { return ['amount' => $a["amount"], 'label' => $a["artist_name"]]; }, $results)
-            // []
-        );
+    // Formatieren aller Songnamen
+    foreach ($results as &$result) {
+        $result["song_name"] = formatName($result["song_name"]);
     }
 
-    function get_question_2($pdo, $artist_name) {
-        $stmt = $pdo->prepare("SELECT song as song_name, COUNT(*) AS amount
-        FROM songs_SRF
-        WHERE artist = '$artist_name'
-        GROUP BY song_name
-        ORDER BY amount DESC
-        LIMIT 4;");
-        $stmt->execute();
-        $results = $stmt->fetchAll();
+    $mostListenedSong = current($results);
+    $mostListenedSongName = $mostListenedSong["song_name"];
+    
+    // Den Index der richtigen Antwort ermitteln
+    $correctIndex = array_search($mostListenedSongName, array_column($results, 'song_name'));
 
+    $answerText = "$mostListenedSongName wird seit dem 8.10.24 am meisten auf SRF 3 gespielt.";
+    
+    return new Question(
+        "Welcher Song von $artist_name wird seit dem 8.10.24 am meisten auf SRF 3 gespielt?",
+        array_map(function($a) { return $a["song_name"]; }, $results),
+        $correctIndex, // Setze den Index der richtigen Antwort
+        $answerText,
+        array_map(function($a) { return ['amount' => $a["amount"], 'label' => $a["song_name"]]; }, $results)
+    );
+}
 
-        $mostListenedSong = current($results);
-        $mostListenedSongName = $mostListenedSong["song_name"];
-        $answerText = "$mostListenedSongName wird seit dem 8.10.24 am meisten auf SRF 3 gespielt.";
-        return new Question(
-            "Welcher Song von $artist_name wird seit dem 8.10.24 am meisten auf SRF 3 gespielt?",
-            array_map(function($a) { return $a["song_name"]; }, $results),
-            $mostListenedSongName,
-            $answerText,
-            array_map(function($a) { return ['amount' => $a["amount"], 'label' => $a["song_name"]]; }, $results)
-        );
-    }
-
-
-
-    function get_question_3($pdo, $song_name, $artist_name) {
-        $stmt = $pdo->prepare("SELECT 
+function get_question_3($pdo, $song_name, $artist_name) {
+    $stmt = $pdo->prepare("SELECT 
         CASE
             WHEN TIME(playtime) BETWEEN '03:00:00' AND '09:00:00' THEN 'Morgen'
             WHEN TIME(playtime) BETWEEN '09:00:00' AND '15:00:00' THEN 'Mittag'
@@ -82,40 +90,44 @@
         WHERE song = \"$song_name\"
         GROUP BY timeofday
         ORDER BY occurrences DESC
-        ");
-        $stmt->execute();
-        $results = $stmt->fetchAll();
+    ");
+    $stmt->execute();
+    $results = $stmt->fetchAll();
 
-        $mostplayeddaytime = current($results);
-        $mostplayeddaytime = $mostplayeddaytime["timeofday"];
-        $answerText = " Am $mostplayeddaytime wird der Song $song_name am meisten gespielt.";
-        return new Question(
-            "Zu welcher Tageszeit glaubst du, wird der Song $song_name von $artist_name am häufigsten auf SRF 3 gespielt?",
-            array_map(function($a) { return $a["timeofday"]; }, $results),
-            $mostListenedSongName,
-            $answerText,
-            array_map(function($a) { return ['amount' => $a["occurrences"], 'label' => $a["timeofday"]]; }, $results)
-        );
-    }
+    // Tageszeiten brauchen keine spezielle Formatierung, da sie als Standardtext vorliegen
 
-    try {
-        $pdo = new PDO($dsn, $username, $password, $options);
-        
-        $first_question = get_question_1($pdo);
-        $second_question = get_question_2($pdo, $first_question->correct);
-        $third_question = get_question_3($pdo, $second_question->correct, $first_question->correct);
+    $mostPlayedTime = current($results);
+    $mostPlayedTimeName = $mostPlayedTime["timeofday"];
+    
+    // Den Index der richtigen Antwort ermitteln
+    $correctIndex = array_search($mostPlayedTimeName, array_column($results, 'timeofday'));
 
-        
-        $questions = [
-            $first_question,
-            $second_question,
-            $third_question
-        ];
+    $answerText = "Am $mostPlayedTimeName wird der Song $song_name am meisten gespielt.";
+    
+    return new Question(
+        "Zu welcher Tageszeit glaubst du, wird der Song $song_name von $artist_name am häufigsten auf SRF 3 gespielt?",
+        array_map(function($a) { return $a["timeofday"]; }, $results),
+        $correctIndex, // Setze den Index der richtigen Antwort
+        $answerText,
+        array_map(function($a) { return ['amount' => $a["occurrences"], 'label' => $a["timeofday"]]; }, $results)
+    );
+}
 
-        echo json_encode($questions); // Gibt die Wetterdaten im JSON-Format aus
-    } catch (PDOException $e) {
-        echo json_encode(['error' => $e->getMessage()]); // Gibt einen Fehler im JSON-Format aus, falls eine Ausnahme auftritt
-    }
+try {
+    $pdo = new PDO($dsn, $username, $password, $options);
+    
+    $first_question = get_question_1($pdo);
+    $second_question = get_question_2($pdo, $first_question->options[$first_question->correct]);
+    $third_question = get_question_3($pdo, $second_question->options[$second_question->correct], $first_question->options[$first_question->correct]);
 
+    $questions = [
+        $first_question,
+        $second_question,
+        $third_question
+    ];
 
+    echo json_encode($questions);
+} catch (PDOException $e) {
+    echo json_encode(['error' => $e->getMessage()]);
+}
 ?>
